f306ad292272feb16d888ef131b3e4e263362ef7
ChenYang Lin <chenyanglinq@gmail.com>
2024-05-10 23:34:00 -0400

    init node

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # IslandEmpire
 
-https://islandempire.onrender.com
+https://islandempire.netlify.app/
---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const app = express();
+
+app.use(express.static('./'))
+
+app.get("/", (req, res) => {
+    res.sendFile("/index.html")
+})
+
+app.listen(3000, () => {
+    console.log("app is running on port 3000");
+})
