ff992fc5c750a37307d94414ba9a37d682fb655c
ChenYang Lin <chenyanglinq@gmail.com>
2024-08-23 15:51:32 -0400

    spinner spin with item and quanlity chance added

--- src/js/windows/Wish.js
+++ src/js/windows/Wish.js
@@ -1,3 +1,4 @@
+import { ENTITY_DATA, ITEM_DATA } from "../GameData.js";
 
 
 export default class Wish {
@@ -35,6 +36,18 @@ export default class Wish {
     initSpinner() {
         this.spinnerContainer = document.getElementById("spinner-container")
         this.spinner = document.getElementById("spinner");
+        this.spinnerBody = document.getElementById("spinner-body");
+        this.spinnerPiece = document.querySelectorAll(".spinner-piece")
+        this.spinnerPieceIcon = document.querySelectorAll(".spinner-piece-icon");
+        this.spinnerPieceIcon.forEach((icon) => {
+            let items = Object.keys(ITEM_DATA);
+            icon.src = this.scene.sys.game.textures.getBase64("item", items[Math.floor(Math.random() * items.length)]);
+        })
+
+        this.spinBtn = document.getElementById("spin-btn");
+        this.spinBtn.addEventListener("pointerdown", () => {
+            this.spinnerSpin();
+        })
 
         this.spinnerScale = 1;
         this.spinnerDegree = 0;
@@ -49,22 +62,75 @@ export default class Wish {
         this.showSpinnerContainer();
         if (this.isSpinning) 
             return;
+
+        // get chance
+        this.quality = "blue";
+        let chance = Math.random();
+        // if (chance < 0.3) {
+        if (chance < 0.1) {
+            // 10% Purple
+            this.quality = "purple";
+        // } else if (chance < 0.6) {
+        } else if (chance < 0.95) {
+            // 85% Blue
+            this.quality = "blue";
+        } else {
+            // 5% Gold
+            this.quality = "gold";
+        }
+
+        
         this.spinnerAcceleration = 0;
+        this.color = "#00c1ff";
         this.spinnerDegree = 0;
+        this.qualitySet = false;
+        this.alpha = 0;
+
         this.spinnerLooper = setInterval(() => {
             this.isSpinning = true;
             spinner.style.transform = `rotate(${this.spinnerDegree}deg) scale(${this.spinnerScale})`
-            spinner.style.borderColor = `blue`;
-            if (this.spinnerAcceleration < 15) 
-                this.spinnerAcceleration += 0.1;
+
+            if (this.spinnerAcceleration < 8)  {
+                this.spinnerAcceleration += 0.2;
+            }
             this.spinnerDegree += this.spinnerAcceleration;
-            if(this.spinnerDegree > 359) {
+            if (this.spinnerDegree > 359) {
                 this.spinnerDegree = 0;
+            }
+
+            if (this.alpha < 255 && !this.qualitySet) {
+                this.alpha += 2;
+                console.log(this.alpha)
+                spinner.style.borderColor = `${this.color}${this.alpha.toString(16)}`
             }
         }, 20);
         setTimeout(() => {
+            this.alpha = 255;
+            this.qualitySet = true;
+            this.spinnerAcceleration = 15;
+
+            spinner.style.borderColor = this.quality;
+            this.spinnerPiece.forEach((element) => {
+                element.classList.add(`spinner-piece-${this.quality}`)
+            });
+            
+            this.spinnerBody.classList.add(`spinner-piece-${this.quality}`);
+        }, 3000)
+        setTimeout(() => {
             clearInterval(this.spinnerLooper);
             this.isSpinning = false; 
+            this.qualitySet = false;
+
+            this.spinnerPiece.forEach((element) => {
+                element.classList.remove("spinner-piece-blue")
+                element.classList.remove("spinner-piece-purple")
+                element.classList.remove("spinner-piece-gold")
+            });
+
+            this.spinnerBody.classList.remove("spinner-piece-blue")
+            this.spinnerBody.classList.remove("spinner-piece-purple")
+            this.spinnerBody.classList.remove("spinner-piece-gold")
+
             this.hideSpinnerContainer();
         }, 5000)
     }
